c765bc958ba2cf259005e690101854c1fdf9d268
drjimmyt <drjimmyt@gmail.com>
2011-11-22 23:20:14 -0800

    created countNeighbors function and did a bit of testing. Note we will probably want to change COLS from 42 to 43 to account for the \0

--- The Game Of Life/theGameOfLife.c
+++ The Game Of Life/theGameOfLife.c
@@ -4,8 +4,8 @@
 #define COLS 42
 
 void readFile(char oldArray[][COLS], int rows, FILE *fileHandle);
-int coundNeighbors(const oldArray[][COLS], int i, int j);
-void calculateNextGeneration(const oldArray[][COLS], newArray[][COLS], int rows);
+int countNeighbors(const char oldArray[][COLS], int i, int j);
+void calculateNextGeneration(const char oldArray[][COLS], char newArray[][COLS], int rows);
 
 int main(void)
 {
@@ -13,6 +13,55 @@ int main(void)
 	char newArray[ROWS][COLS];
 
 
+	char testArray[ROWS][COLS] = 
+	{{"                                         "},
+	{"                                         "},
+	{"                                         "},
+	{"                                         "},
+	{"        ***                              "},
+	{"        ***                              "},
+	{"        ***                              "},
+	{"                                         "},
+	{"                                         "},
+	{"                                         "},
+	{"                                         "},
+	{"                                         "},
+	{"                                         "},
+	{"                                         "},
+	{"                                         "},
+	{"                                         "},
+	{"                                         "},
+	{"                                         "},
+	{"                                         "},
+	{"                                         "},
+	{"                                         "},
+	{"                                         "}};
 
+	printf("%d",countNeighbors(testArray, 5, 9));
+
+	getchar();
 	return EXIT_SUCCESS;
+}
+
+int countNeighbors(const char oldArray[][COLS], int i, int j)
+{
+	int row = i - 1; //the row above row i
+	int column = j - 1; // the column left of column j
+	int neighborCount = 0;
+	char lifeChar = '*';
+
+	// go through each row and column on the 3x3 grid of neighbors. 
+	// Keep count of the neighbors
+	// ignoring the center (or origional point i,j)
+	for (row = (i - 1); row <= (i + 1); row++)
+	{
+		for (column = (j - 1); column <= (j + 1); column++)
+		{
+			if (row == i && column == j)
+				;//do nothing
+			else if (oldArray[row][column] == lifeChar)
+				neighborCount++;
+		}
+	}
+	return neighborCount;	
 }
